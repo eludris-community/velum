2ef6dfed6e14e56033c0bcd7453021fae494bb7d
Emre Terzioglu <50607143+EmreTech@users.noreply.github.com>
2022-11-22 12:17:52 -0800

    fix: stop making classmethod generic at runtime (#1)
    * fix: stop making classmethod generic at runtime
    
    * chore: remove redundant slotscheck exclude
    
    Co-authored-by: Chromosomologist <milan.smts@gmail.com>

--- velum/internal/class_utils.py
+++ velum/internal/class_utils.py
@@ -1,13 +1,5 @@
 import typing
 
-if not typing.TYPE_CHECKING:
-    # Ensure classmethod is generic, like typing suggests...
-
-    class classmethod(classmethod):
-        def __class_getitem__(cls, *args):
-            return cls
-
-
 T = typing.TypeVar("T")
 ClsT = typing.TypeVar("ClsT")
 
@@ -17,7 +9,7 @@ class classproperty(typing.Generic[ClsT, T]):
     __slots__: typing.Sequence[str] = ("callback",)
 
     def __init__(self, callback: typing.Callable[[typing.Type[ClsT]], T]):
-        self.callback = typing.cast(classmethod[T], callback)
+        self.callback = typing.cast("classmethod[T]", callback)
 
     def __get__(self, instance: typing.Optional[ClsT], owner: typing.Type[ClsT]) -> T:
         return self.callback.__func__(owner)  # type: ignore
