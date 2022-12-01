c7c13e911ba8c21ef1c1cfa7f1688f9afc7b3718
Chromosomologist <milan.smts@gmail.com>
2022-12-01 16:31:36 +0100

    chore: minor local event changes

--- velum/__init__.py
+++ velum/__init__.py
@@ -2,8 +2,8 @@ from velum import events as events
 from velum.errors import *
 from velum.events import Event as Event
 from velum.events import ExceptionEvent as ExceptionEvent
-from velum.events.message_events import *
 from velum.events.connection_events import *
+from velum.events.message_events import *
 from velum.impl.bot import GatewayBot as GatewayBot
 from velum.internal.data_binding import JSONImpl as JSONImpl
 from velum.internal.data_binding import set_json_impl as set_json_impl
