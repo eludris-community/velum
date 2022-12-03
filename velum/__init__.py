39fe65950bc8406fe39c5f08bd4da51faa3369c0
Chromosomologist <milan.smts@gmail.com>
2022-12-03 03:31:00 +0100

    feat: implement file types

--- velum/__init__.py
+++ velum/__init__.py
@@ -4,6 +4,7 @@ from velum.events import Event as Event
 from velum.events import ExceptionEvent as ExceptionEvent
 from velum.events.connection_events import *
 from velum.events.message_events import *
+from velum.files import *
 from velum.impl.bot import GatewayBot as GatewayBot
 from velum.internal.data_binding import JSONImpl as JSONImpl
 from velum.internal.data_binding import set_json_impl as set_json_impl
--- velum/internal/async_utils.py
+++ velum/internal/async_utils.py
@@ -1,10 +1,13 @@
 import asyncio
+import inspect
 import typing
 
 __all__: typing.Sequence[str] = (
     "create_completed_future",
     "cancel_futures",
     "first_completed",
+    "is_async_iterator",
+    "is_async_iterable",
 )
 
 
@@ -42,3 +45,14 @@ async def first_completed(
         raise
     finally:
         await cancel_futures(futures)
+
+
+def is_async_iterator(obj: typing.Any) -> typing.TypeGuard[typing.AsyncIterator[object]]:
+    """Determine if the object is an async iterator or not."""
+    return asyncio.iscoroutinefunction(getattr(obj, "__anext__", None))
+
+
+def is_async_iterable(obj: typing.Any) -> typing.TypeGuard[typing.AsyncIterable[object]]:
+    """Determine if the object is an async iterable or not."""
+    attr = getattr(obj, "__aiter__", None)
+    return inspect.isfunction(attr) or inspect.ismethod(attr)
