6b0087f83ff522dc3ed15f5cb14428a450b322db
Chromosomologist <chromosomologist@gmail.com>
2023-05-24 14:46:40 +0200

    feat(gateway): use hello data for heartbeat

--- velum/impl/gateway.py
+++ velum/impl/gateway.py
@@ -34,7 +34,6 @@ if typing.TYPE_CHECKING:
 
 _BACKOFF_WINDOW: typing.Final[float] = 15.0
 _GATEWAY_URL: typing.Final[str] = "wss://ws.eludris.gay"
-_HEARTBEAT_INTERVAL: typing.Final[float] = 45.0
 
 # Payload attributes.
 _OP: typing.Final[str] = sys.intern("op")
@@ -43,6 +42,7 @@ _D: typing.Final[str] = sys.intern("d")
 # Special opcodes.
 _PONG: typing.Final[str] = sys.intern("PONG")
 _PING: typing.Final[str] = sys.intern("PING")
+_HELLO: typing.Final[str] = sys.intern("HELLO")
 
 
 class GatewayWebsocket:
@@ -268,6 +268,27 @@ class GatewayHandler(gateway_trait.GatewayHandler):
         self._is_closing = False
         self._logger.info("Gateway connection closed successfully.")
 
+    async def _poll_hello_event(self) -> float:
+        assert self._gateway_ws is not None
+        assert self._connection_event is not None
+
+        payload = await self._gateway_ws.receive_json()
+        op = payload[_OP]
+
+        if op != _HELLO:
+            self._logger.debug(
+                "Expected %r opcode, received %r. Closing connection.",
+                _HELLO,
+                op,
+            )
+            # TODO: Custom exception and don't use magic number.
+            await self._gateway_ws.send_close(code=1002, message=b"Expected HELLO op.")
+            raise RuntimeError(f"Expected opcode {_HELLO}, received {op} instead.")
+
+        # TODO: Maybe return fully deserialised event and use ratelimit info.
+        #       For now, only using the heartbeat interval will do.
+        return float(payload[_D]["heartbeat_interval"]) / 1_000.0
+
     async def _poll_events(self) -> None:
         assert self._gateway_ws is not None
         assert self._connection_event is not None
@@ -349,7 +370,9 @@ class GatewayHandler(gateway_trait.GatewayHandler):
             logger=self._logger, url=self._gateway_url
         )
 
-        heartbeat_task = asyncio.create_task(self._heartbeat(), name="heartbeat")
+        heartbeat_interval = await self._poll_hello_event()
+
+        heartbeat_task = asyncio.create_task(self._heartbeat(heartbeat_interval), name="heartbeat")
         poll_events_task = asyncio.create_task(self._poll_events(), name="poll events")
 
         # Indicate connection logic is done.
@@ -366,11 +389,11 @@ class GatewayHandler(gateway_trait.GatewayHandler):
 
         self._last_heartbeat = time.monotonic()
 
-    async def _heartbeat(self) -> None:
+    async def _heartbeat(self, heartbeat_interval: float) -> None:
         assert self._gateway_ws is not None
 
         self._last_heartbeat_ack = time.monotonic()
-        self._logger.debug("Starting heartbeat with interval %s [s].", _HEARTBEAT_INTERVAL)
+        self._logger.debug("Starting heartbeat with interval %s [s].", heartbeat_interval)
 
         while True:
 
@@ -383,4 +406,4 @@ class GatewayHandler(gateway_trait.GatewayHandler):
                 return
 
             await self._send_heartbeat()
-            await asyncio.sleep(_HEARTBEAT_INTERVAL)
+            await asyncio.sleep(heartbeat_interval)
