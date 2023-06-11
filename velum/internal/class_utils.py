1e2704713a82709e364ee72977bdc95c9bdc25b8
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-06-11 19:48:54 +0100

    fix(internal): fully type classproperty's callback (#8)
    * fix(internal): fully type classproperty's callback
    
    * chore(deps-dev): bump pyright to 1.1.313

--- velum/internal/class_utils.py
+++ velum/internal/class_utils.py
@@ -11,10 +11,10 @@ class classproperty(typing.Generic[_ClsT, _T]):
 
     __slots__: typing.Sequence[str] = ("callback",)
 
-    callback: "classmethod[_T]"
+    callback: "classmethod[_ClsT, ..., _T]"
 
     def __init__(self, callback: typing.Callable[[typing.Type[_ClsT]], _T]):
-        self.callback = typing.cast("classmethod[_T]", callback)
+        self.callback = typing.cast("classmethod[_ClsT, ..., _T]", callback)
 
     def __get__(self, instance: typing.Optional[_ClsT], owner: typing.Type[_ClsT]) -> _T:
         return self.callback.__func__(owner)  # type: ignore
