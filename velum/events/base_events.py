ef979abf0ddcded4351483142e9e1526d16ed2ce
Chromosomologist <chromosomologist@gmail.com>
2023-10-24 23:40:03 +0200

    refactor(events): optimise event creation logic

--- velum/events/base_events.py
+++ velum/events/base_events.py
@@ -5,8 +5,6 @@ import typing
 
 import attr
 
-from velum.internal import class_utils
-
 __all__: typing.Sequence[str] = ("Event", "ExceptionEvent")
 
 
@@ -14,44 +12,30 @@ EventT = typing.TypeVar("EventT", bound="Event")
 EventCallbackT = typing.Callable[[EventT], typing.Coroutine[typing.Any, typing.Any, None]]
 
 
-_id_counter = 0
-
-
 class Event(abc.ABC):
     """Base type for all events"""
 
     __slots__ = ()
 
-    __bitmask: int
-    __dispatches: tuple[type[Event], ...]
+    __id_counter: int = 0
+
+    bitmask: int
+    dispatches: tuple[type[Event], ...]
 
     def __init_subclass__(cls) -> None:
         super().__init_subclass__()
-
-        mro = cls.mro()
-        cls.__dispatches = tuple(sub_cls for sub_cls in mro if issubclass(sub_cls, Event))
 
         if "__attrs_attrs__" in cls.__dict__:
             # attrs runs __new__ a second time on class creation, and we don't
             # need to increment the bitmask again.
             return
 
-        global _id_counter  # noqa: PLW0603  # NOTE: maybe make this a private attr? hmm...
-
         # We don't have to explicitly include Event here as issubclass(Event, Event) returns True.
         # Non-event classes should be ignored.
-        cls.__bitmask = 1 << _id_counter
-        _id_counter += 1
+        cls.dispatches = tuple(sub_cls for sub_cls in cls.mro() if issubclass(sub_cls, Event))
+        cls.bitmask = 1 << cls.__id_counter
 
-    @class_utils.classproperty
-    @classmethod
-    def bitmask(cls) -> int:
-        return cls.__bitmask
-
-    @class_utils.classproperty
-    @classmethod
-    def dispatches(cls) -> tuple[type[Event], ...]:
-        return cls.__dispatches
+        cls.__id_counter += 1
 
 
 # Set event parameters on the actual event class.
